6925b310658ecf67725d25a1ba0c95dc6740cd62
brandon197 <pinkzepp@my.yorku.ca>
2021-01-05 19:59:55 -0500

    put images into Grid Form

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
 import React from "react";
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
   console.log(props.images);
-  const imagList = props.images.map(({ description, id, urls }) => {
-    return <img key={id} alt={description} src={urls.regular}></img>;
+  const imagList = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image}></ImageCard>;
   });
-  return <div>{imagList}</div>;
+  return <div className="image-list">{imagList}</div>;
 };
 
 export default ImageList;
